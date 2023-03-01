f8815902bb0158c6f5a54279f5457a1a7eebc39a
LuanPetruitis <luanpetruitis@hotmail.com>
2023-03-01 11:00:26 -0300

    teste

--- meu_primeiro_modulo/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import meu_primeiro_modelo
--- meu_primeiro_modulo/models/meu_primeiro_modelo.py
+++
@@ -1,5 +0,0 @@
-from odoo import models
-
-class MeuPrimeiroModulo(models.Model):
-    _name = 'meu.primeiro.modelo'
-    _description = "meu primeiro modelo criado no Odoo"
